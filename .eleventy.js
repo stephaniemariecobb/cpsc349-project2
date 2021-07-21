00a6e01bc75f377e63b6b3a9fe2dd9c1b8be5176
stephaniemariecobb <55161389+stephaniemariecobb@users.noreply.github.com>
2021-07-21 16:26:05 -0700

    Make text visible on dark backgrounds, and placeholder .css files.

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("styles/main.css");
+    eleventyConfig.addPassthroughCopy("styles/flexbox.css");
+    eleventyConfig.addPassthroughCopy("styles/grid.css");
 };
